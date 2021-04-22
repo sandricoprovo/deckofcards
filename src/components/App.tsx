c283a0b8b4b40e3d43130e05f7431cba60adf4ed
Sandrico Provo <Sprovo@outlook.com>
2021-04-22 13:52:19 -0300

    [test] Snowpack fully working with typescript

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,7 +7,7 @@ const App: React.FC<any> = () => {
     // Return html structure
     return (
         <div>
-            <h1>Hello World Crazy Fast</h1>
+            <h1>Hello World</h1>
         </div>
     );
 };
